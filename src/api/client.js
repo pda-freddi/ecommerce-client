d6e4a7205107340ecd4a884060178c3dd2b6c48b
Pedro Freddi <pdafreddi@gmail.com>
2022-08-16 17:30:16 -0300

    Add client.js to interface with the backend API

---
+++ src/api/client.js
@@ -0,0 +1,97 @@
+/*
+*  Simple client object exposing methods that wrap the fetch method and interact
+*  with the e-commerce back-end
+*/
+
+const baseUrl = "http://localhost:8000/api";
+
+export const client = {
+  get: async (endpoint) => {
+      try {
+        const response = await fetch(`${baseUrl}${endpoint}`, { credentials: "include" });
+        if (!response.ok) {
+          const error = await response.json();
+          return { error: true, data: error.message };
+        }
+        const responseData = await response.json();
+        return { error: false, data: responseData };
+      } catch(error) {
+        return { error: true, data: error.message };
+      }
+    },
+  post: async (endpoint, payload, expectResBody) => {
+    try {
+      const response = await fetch(
+        `${baseUrl}${endpoint}`, 
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(payload ? payload : "")
+        });
+      if (!response.ok) {
+        const error = await response.json();
+        return { error: true, data: error.message };
+      }
+      if (expectResBody) {
+        const responseData = await response.json();
+        return { error: false, data: responseData };
+      }
+      return { error: false, data: null }
+    } catch(error) {
+      return { error: true, data: error.message };
+    }
+  },
+  put: async (endpoint, payload, expectResBody) => {
+    try {
+      const response = await fetch(
+        `${baseUrl}${endpoint}`, 
+        {
+          method: "PUT",
+          credentials: "include",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(payload ? payload : "")
+        });
+      if (!response.ok) {
+        const error = await response.json();
+        return { error: true, data: error.message };
+      }
+      if (expectResBody) {
+        const responseData = await response.json();
+        return { error: false, data: responseData };
+      }
+      return { error: false, data: null }
+    } catch(error) {
+      return { error: true, data: error.message };
+    }
+  },
+  delete: async (endpoint, payload, expectResBody) => {
+    try {
+      const response = await fetch(
+        `${baseUrl}${endpoint}`, 
+        {
+          method: "DELETE",
+          credentials: "include",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(payload ? payload : "")
+        });
+      if (!response.ok) {
+        const error = await response.json();
+        return { error: true, data: error.message };
+      }
+      if (expectResBody) {
+        const responseData = await response.json();
+        return { error: false, data: responseData };
+      }
+      return { error: false, data: null }
+    } catch(error) {
+      return { error: true, data: error.message };
+    }
+  }
+};
