cf54f8c370eb460e43df711f6c9ef50351aa3d97
Pedro Freddi <pdafreddi@gmail.com>
2022-08-23 20:01:08 -0300

    Add component that ensures authentication for restricted routes

---
+++ src/components/utils/ensure-authentication/EnsureAuthentication.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import styles from "./EnsureAuthentication.module.css";
+
+const EnsureAuthentication = ({ children, showMessage }) => {
+  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    if (!isAuthenticated && showMessage) {
+      setTimeout(() => {
+        navigate("/login", { state: { from: location }, replace: true })
+      }, 3000);
+    }
+  });
+
+  if (!isAuthenticated && showMessage) {
+    return (
+      <section className={styles.container}>
+        <p className={styles.errorMessage}>You must be authenticated to access this page.</p>
+        <p>Redirecting to login...</p>
+        <ClipLoader />
+      </section>
+    );
+  } else if (!isAuthenticated) {
+    return navigate("/login", { state: { from: location }, replace: true });
+  }
+
+  return children;
+};
+
+export default EnsureAuthentication;
