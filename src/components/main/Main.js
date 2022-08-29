484735eb8c956943e2a634efc0b7c54637151d96
Pedro Freddi <pdafreddi@gmail.com>
2022-08-29 14:38:26 -0300

    Add a RouteConfig component to centralize the routes handling

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,38 +1,11 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import CategoryList from "../category/category-list/CategoryList.js";
-import ProductList from "../product/product-list/ProductList.js";
-import ProductPage from "../product/product-page/ProductPage.js";
-import CustomerIndex from "../customer/customer-index/CustomerIndex.js";
-import CustomerUpdatePage from "../customer/customer-update/CustomerUpdatePage.js";
-import CustomerDeletePage from "../customer/customer-delete/CustomerDeletePage.js";
-// import OrdersPage from "../order/orders-page/OrdersPage.js";
-import LoginPage from "../login/login-page/LoginPage.js";
-import RegisterPage from "../register/register-page/RegisterPage.js";
-import CartPage from "../cart/cart-page/CartPage.js";
-import CheckoutPage from "../checkout/checkout-page/CheckoutPage.js";
+import RoutesConfig from "./RoutesConfig.js";
 import styles from "./Main.module.css";
 
 const Main = () => {
   return (
     <main className={styles.container}>
-      <Routes>
-        <Route path="/" element={<CategoryList />}>
-          <Route index element={<ProductList />} />
-          <Route path="categories/:categoryName" element={<ProductList />}/ >
-          <Route path="products" element={<ProductList />} />
-          <Route path="products/:productName" element={<ProductPage />} />
-        </Route>
-        <Route path="/my-account" element={<CustomerIndex />} />
-        <Route path="/my-account/update" element={<CustomerUpdatePage />} />
-        <Route path="/my-account/delete" element={<CustomerDeletePage />} />
-        {/* <Route path="/my-account/orders" element={<OrdersPage />} /> */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/cart/checkout" element={<CheckoutPage />} />
-        <Route path="*" element={<p>Not found!</p>} />
-      </Routes>
+      <RoutesConfig />
     </main>
   );
 };
---
+++ src/components/main/RoutesConfig.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import CategoryList from "../category/category-list/CategoryList.js";
+import ProductList from "../product/product-list/ProductList.js";
+import ProductPage from "../product/product-page/ProductPage.js";
+import CustomerIndex from "../customer/customer-index/CustomerIndex.js";
+import CustomerUpdatePage from "../customer/customer-update/CustomerUpdatePage.js";
+import CustomerDeletePage from "../customer/customer-delete/CustomerDeletePage.js";
+import OrderHistoryPage from "../order/order-history-page/OrderHistoryPage.js";
+import OrderPage from "../order/order-page/OrderPage.js";
+import LoginPage from "../login/login-page/LoginPage.js";
+import RegisterPage from "../register/register-page/RegisterPage.js";
+import CartPage from "../cart/cart-page/CartPage.js";
+import CheckoutPage from "../checkout/checkout-page/CheckoutPage.js";
+import NotFound from "../not-found/NotFound.js";
+
+const RoutesConfig = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<CategoryList />}>
+        <Route index element={<ProductList />} />
+        <Route path="categories/:categoryName" element={<ProductList />}/ >
+        <Route path="products" element={<ProductList />} />
+        <Route path="products/:productName" element={<ProductPage />} />
+      </Route>
+      <Route path="/my-account" element={<CustomerIndex />} />
+      <Route path="/my-account/update" element={<CustomerUpdatePage />} />
+      <Route path="/my-account/delete" element={<CustomerDeletePage />} />
+      <Route path="/my-account/orders" element={<OrderHistoryPage />} />
+      <Route path="/my-account/orders/:orderId" element={<OrderPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/cart/checkout" element={<CheckoutPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default RoutesConfig;
