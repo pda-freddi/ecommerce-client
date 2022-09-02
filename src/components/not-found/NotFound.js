880154240a3117c95986f931916ad24488c3e678
Pedro Freddi <pdafreddi@gmail.com>
2022-09-02 15:08:23 -0300

    Style adjustments to NotFound component

--- src/components/not-found/NotFound.js
+++ src/components/not-found/NotFound.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import notFoundIcon from "../../icons/not-found.png";
+import BackButton from "../utils/back-button/BackButton.js";
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
-
-  const navigate = useNavigate();
-  
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Not Found</h2>
-      <img src={notFoundIcon} alt="404 error icon" className={styles.icon} />
+      <img src={notFoundIcon} alt="404 error icon" />
       <p className={styles.message}>Sorry, can't find the requested page.</p>
-      <button className={styles.button} onClick={() => navigate(-1)}>
-        Back
-      </button>
+      <BackButton destination={-1}>Back</BackButton>
     </div>
   );
 };
