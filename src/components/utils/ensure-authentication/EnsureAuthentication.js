082cbb1fdccf8a0818d654e7204239644d1b7fec
Pedro Freddi <pdafreddi@gmail.com>
2022-09-02 12:05:16 -0300

    Style adjustments to EnsureAuthentication component

--- src/components/utils/ensure-authentication/EnsureAuthentication.js
+++ src/components/utils/ensure-authentication/EnsureAuthentication.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
+import warningIcon from "../../../icons/warning.png";
 import { ClipLoader } from "react-spinners";
 import styles from "./EnsureAuthentication.module.css";
 
@@ -8,8 +9,11 @@ const EnsureAuthentication = ({ children, showMessage }) => {
   
   const navigate = useNavigate();
   const location = useLocation();
+
+  // Get authentication status from the store
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
+  // Set up a redirect to login page if user is not authenticated
   useEffect(() => {
     if (!isAuthenticated && showMessage) {
       const timerId = setTimeout(() => {
@@ -19,10 +23,14 @@ const EnsureAuthentication = ({ children, showMessage }) => {
     }
   });
 
+  // Render the error message if showMessage prop is true
   if (!isAuthenticated && showMessage) {
     return (
       <section className={styles.container}>
-        <p className={styles.errorMessage}>You must be authenticated to access this page.</p>
+        <p className={styles.errorMessage}>
+          <img src={warningIcon} alt="Warning" className={styles.icon} />
+          You must be authenticated to access this page.
+        </p>
         <p>Redirecting to login...</p>
         <ClipLoader />
       </section>
